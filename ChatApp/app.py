c23f2e03e5be4c0e523246fb82619334b43311fa
Taka51期 <sh.takanori@gmail.com>
2025-07-24 00:56:39 +0900

    app.py追加

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -1,2 +1,44 @@
 from flask import Flask, request, redirect, render_template, session, flash, abort, url_for
-from
+from datetime import timedelta
+import hashlib
+import uuid
+import re
+import os
+
+from models import User, Channel, Messsage
+from util.assets import bundle_css_files
+
+
+# 定数定義
+
+# ブラウザに静的ファイル（CSSや画像など）を長くキャッシュさせる設定。
+# 開発中は変更がすぐに反映されないことがあるため、コメントアウトするのが無難です。
+
+# 複数のCSSファイルを1つにまとめて圧縮（バンドル）する処理を実行。
+
+# ルートページのリダイレクト処理
+
+# サインアップページの表示
+
+# サインアップ処理
+
+# ログインページの表示
+
+# ログイン処理
+
+# ログアウト
+
+# チャンネル一覧ページの表示
+
+# チャンネルの作成
+
+# チャンネルの更新
+
+# チャンネルの削除
+
+# チャンネル詳細ページの表示（各チャンネル内で、そのチャンネルに属している全メッセージを表示させる）
+
+# メッセージの投稿
+
+# メッセージの削除
+
