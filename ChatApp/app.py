be8875ea2461a89cbf4bacea12c5821fc44219b6
Taka51期 <sh.takanori@gmail.com>
2025-07-25 19:31:28 +0900

    app.py作業続き

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -10,11 +10,22 @@ from util.assets import bundle_css_files
 
 
 # 定数定義
+EMAIL_PATTERN = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+SESSION_DAYS = 30
 
+app = Flask(__name__)
+app.secret.key = os.getenv('SECRET_KEY', uuid.uuid4().hex)
+app.permanent_session_lifetime = timedelta(days=SESSION_DAYS)
+
+                                           
 # ブラウザに静的ファイル（CSSや画像など）を長くキャッシュさせる設定。
 # 開発中は変更がすぐに反映されないことがあるため、コメントアウトするのが無難です。
+app.config('SEND_FILE_MAX_AGE_DEFAULT') = 2678400
+
 
 # 複数のCSSファイルを1つにまとめて圧縮（バンドル）する処理を実行。
+bundle_css_files(app)
+
 
 # ルートページのリダイレクト処理
 
