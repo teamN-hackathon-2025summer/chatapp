a355b0f3635267501579d4d62a61f5cc4d162f74
Taka51期 <sh.takanori@gmail.com>
2025-08-24 22:12:25 +0900

    エラー確認用ルート追加、500.html修正

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -239,6 +239,12 @@ def like_function(cid,message_id):
         Like.like_insert(uid,message_id)
     
     return redirect(url_for('detail', cid=cid))
+
+# エラー確認用 
+# 本番ではここはコメントアウト
+@app.route('/test_error')
+def test_error():
+    return render_template('error/500.html')
         
 
 
